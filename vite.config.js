2012f87345cc4ab59af04e69f8cce2457bb5d6b8
qilip <qilip@qilip.io>
2022-06-26 04:48:11 +0900

    Fix api url

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.exit.moe',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  plugins: [vue()]
 })
