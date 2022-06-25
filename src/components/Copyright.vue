ed60f111a7659dae606119ca823300b12fe12955
qilip <qilip@qilip.io>
2022-06-26 00:35:11 +0900

    Improve UI

--- src/components/Copyright.vue
+++ src/components/Copyright.vue
@@ -1,18 +1,18 @@
 <template>
   <Footer>
-    <div class="container mx-auto items-center my-4 flex">
-        <div class="m-auto flex flex-col text-base text-gray-500 space-y-2">
-          <p>
-            <b>exit.moe</b> by <a href="https://qilip.io">qilip</a>
-          </p>
-          <div class="m-auto">
-            <a href="https://github.com/qilip/exit-moe">
-              <svg class="stroke-gray-500" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
-              </svg>
-            </a>
-          </div>
+    <div class="container mx-auto items-center my-4 flex font-light">
+      <div class="m-auto flex flex-col text-base text-gray-500 space-y-2">
+        <p>
+          <b class="font-medium">exit.moe</b> by <a href="https://qilip.io">qilip</a>
+        </p>
+        <div class="m-auto">
+          <a href="https://github.com/qilip/exit-moe">
+            <svg class="stroke-gray-500" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
+            </svg>
+          </a>
         </div>
+      </div>
     </div>
   </Footer>
 </template>
--- src/components/UrlInput.vue
+++ src/components/UrlInput.vue
@@ -6,9 +6,9 @@
     <div class="flex justify-center mb-4">
       <input type="text" spellcheck="false" placeholder="https://example.com" autofocus
       class="w-full border-2 border-gray-300 hover:border-gray-400
-      focus:outline-none focus:border-gray-500 text-gray-700 rounded text-lg pl-3 pr-3 py-2"/>
-      <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-2 py-3 px-6 hover:bg-gray-200 rounded">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+      focus:outline-none focus:border-gray-500 text-gray-700 rounded-lg text-lg pl-3 pr-3 py-2"/>
+      <button class="ml-4 inline-flex text-gray-700 py-3 px-6 shadow-lg bg-cyan-300 hover:bg-cyan-400 shadow-cyan-200 rounded-lg" @click="shorten">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
         </svg>
       </button>
@@ -17,9 +17,25 @@
       <p>
         링크는 30일 뒤 만료됩니다.
       </p>
-      <div class="text-2xl bg-gray-100 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
-        https://exit.moe/SxWlI1
-      </div>
+      <button class="shortened-link text-2xl inline-flex py-3 px-5 rounded-lg shadow-lg items-center focus:outline-none flex"
+      v-bind:class="[
+        beforeCopy ? 'bg-gray-100 hover:bg-gray-200 shadow-gray-100' : 'bg-green-300 hover:bg-green-400 shadow-green-200',
+        isShortened ? '' : 'invisible'
+      ]"
+      @click="linkCopy">
+        {{ shortenedLink }}
+        <div class="ml-3" v-if="beforeCopy">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+            <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+          </svg>
+        </div>
+        <div class="ml-3" v-else>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+          </svg>
+        </div>
+      </button>
     </div>
   </div>
 </template>
@@ -27,5 +43,22 @@
 <script>
 export default {
   name:'UrlInput',
+  data() {
+    return {
+      shortenedLink: 'shortenedURL',
+      isShortened: false,
+      beforeCopy: true,
+    }
+  },
+  methods: {
+    linkCopy() {
+      navigator.clipboard.writeText(this.shortenedLink);
+      this.beforeCopy = false;
+    },
+    shorten() {
+      this.shortenedLink = 'https://exit.moe/SxWlI12';
+      this.isShortened = true;
+    }
+  }
 }
 </script>
